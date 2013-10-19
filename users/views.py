07346ee8c4cd6d0ddfae2d3e25ad67a738164fe0
NewsCred Nazmul <newscred@NewsCred-air-16.local>
2013-10-19 14:01:58 +0600

    reset password and signup functionality updated

--- users/views.py
+++ users/views.py
@@ -32,6 +32,32 @@ def login(request):
                 else:
                     msg = 'Try with valid password'
                     return render(request, 'user/home.html', {'error_msg': msg})
+
+def registration(request):
+    name = request.POST['name']
+    email = request.POST['email']
+    password = request.POST['password']
+
+    if name != '' and email != '' and password != '':
+        try:
+            officer = user.objects.get(email = email)
+        except ObjectDoesNotExist:
+            officer = user(name = name, email = email, password = password)
+            officer.save()
+            try:
+                officer = user.objects.get(email = email)
+            except ObjectDoesNotExist:
+                msg = 'Try Later.'
+            else:
+                msg = 'Congratualtions!! Log in ...'
+        else:
+            msg = 'Email already exists. Try another ...'
+    else:
+        msg = 'Fill empty fields.'
+    return render(request, 'user/home.html', {'reg_msg': msg,})
+
+
+
 def logout(request):
     del request.session['officer_id']
     return redirect('/')
@@ -41,20 +67,38 @@ def home(request):
         if request.session['officer_id']:
             return redirect('/officer')
     except KeyError:
-        if request.GET.get('login'):
+        if request.GET.get('login') == 'login':
             return login(request)
+        elif request.GET.get('reg') == 'registration':
+                return registration(request)
         else:
             return render(request, 'user/home.html')
 
 
+def reset_password(request, officer):
+    old_password = request.POST['old-password']
+    new_password = request.POST['new-password']
+
+    if officer.password == old_password:
+        officer.password = new_password
+        officer.save()
+        if officer.password == new_password:
+            msg = 'Your password changed successfully.'
+            return msg
+    else:
+        msg = 'Enter correct current password.'
+        return msg
 
 def officer(request):
     try:
         if request.session['officer_id']:
             officer = user.objects.get(pk = request.session['officer_id'])
-            print officer.name
-            print officer.email
-            return render(request, 'user/index.html', {'user': officer.name, 'email': officer.email, 'password':officer.password})
+            if request.GET.get('reset') == 'password':
+                msg = reset_password(request, officer)
+            else:
+                msg = ''
+
+            return render(request, 'user/index.html', {'user': officer.name, 'email': officer.email, 'message':msg,})
     except KeyError:
         return redirect('/')
 
