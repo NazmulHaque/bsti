204dbc3731f5326b68d725cb59f8cec56a3fd4b6
NewsCred Nazmul <newscred@NewsCred-air-16.local>
2013-10-19 12:45:16 +0600

    login-logout function updated

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from users import views
 
 urlpatterns = patterns('',
-    url(r'^officer', views.login, name='officer'),
+    url(r'^officer$', views.officer, name='officer'),
+    url(r'^logout$', views.logout, name='logout'),
     url(r'^$', views.home, name='home'),
 )
--- users/views.py
+++ users/views.py
@@ -1,34 +1,60 @@
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.shortcuts import redirect
+# from django.contrib.sessions.models import Session
 from django.utils.datastructures import MultiValueDictKeyError
 from django.core.exceptions import ObjectDoesNotExist
 from users.models import user
 
-def home(request):
-    return render(request, 'user/home.html')
-
 def login(request):
     try:
-        email = request.POST['email']
-        password = request.POST['password']
-
-    except MultiValueDictKeyError:
-        msg = 'Try with valid email & password'
-        return redirect('/', {'error_msg': msg})
-
-    else:
+        if request.session['officer_id']:
+            return redirect('/officer')
+    except KeyError:
         try:
-            officer = user.objects.get(email = email)
-        except ObjectDoesNotExist:
-            msg = 'Try with valid email.'
-            return render(request, 'user/home.html', {'error_msg': msg})
+            email = request.POST['email']
+            password = request.POST['password']
+
+        except MultiValueDictKeyError:
+            msg = 'Try with valid email & password'
+            render(request, 'user/home.html', {'error_msg': msg})
         else:
-            user_password = officer.password
-            if user_password == password:
-                request.session['officer_id'] = officer.id
-                return render(request, 'user/index.html', {'user': officer.name, 'email': officer.email, 'password':officer.password})
-            else:
-                msg = 'Try with valid password'
+            try:
+                officer = user.objects.get(email = email)
+            except ObjectDoesNotExist:
+                msg = 'Try with valid email.'
                 return render(request, 'user/home.html', {'error_msg': msg})
+            else:
+                user_password = officer.password
+                if user_password == password:
+                    request.session['officer_id'] = officer.id
+                    return redirect('/officer')
+                else:
+                    msg = 'Try with valid password'
+                    return render(request, 'user/home.html', {'error_msg': msg})
+def logout(request):
+    del request.session['officer_id']
+    return redirect('/')
+
+def home(request):
+    try:
+        if request.session['officer_id']:
+            return redirect('/officer')
+    except KeyError:
+        if request.GET.get('login'):
+            return login(request)
+        else:
+            return render(request, 'user/home.html')
+
+
+
+def officer(request):
+    try:
+        if request.session['officer_id']:
+            officer = user.objects.get(pk = request.session['officer_id'])
+            print officer.name
+            print officer.email
+            return render(request, 'user/index.html', {'user': officer.name, 'email': officer.email, 'password':officer.password})
+    except KeyError:
+        return redirect('/')
 
